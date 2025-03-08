7afd2319b7f6b3660fe62ecdaf991ca956651768
Bpin0102s <nepal0009777@gmail.com>
2025-03-07 23:30:39 -0500

    all pages done

---
+++ Farm/Farm/SignInPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Farm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login successful!", "OK");
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ Farm/Farm/Signup.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Farm
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Signup : ContentPage
+	{
+		public Signup ()
+		{
+			InitializeComponent ();
+		}
+        private void OnSignUpClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Sign Up", "Account created successfully!", "OK");
+            Navigation.PushAsync(new Login());
+        }
+    }
+}
